418e600b432cbaff500954f75e0be936f47da81d
Nakato-Musana-Zaina <nakatozainamusana@gmail.com>
2024-07-16 10:08:44 +0300

    mades changes

--- classes2/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Classes2Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'classes2'
--- classperiod/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import ClassPeriod
-from .serializers import ClassPeriodSerializer
-
-class ClassPeriodViewSet(viewsets.ModelViewSet):
-    queryset = ClassPeriod.objects.all()
-    serializer_class = ClassPeriodSerializer
